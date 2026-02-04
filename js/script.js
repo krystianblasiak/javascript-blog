6137a92e72b1fbc5dcc1b4334dc1cb12021c2aeb
Krystian Błasiak <blasiakkrystian@o2.pl>
2026-02-04 17:54:23 +0100

    Add class 'active' to the clicked link in function titleClickHandler in file script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 'use strict';
 
 function titleClickHandler(event){
+    const clickedElement = this;
+
     console.log("Link was clicked!");
     console.log(event)
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll(".titles a.active");
 
@@ -12,9 +14,11 @@ function titleClickHandler(event){
         activeLink.classList.remove("active");
     }
 
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+    
+    clickedElement.classList.add("active");
 
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
     
     const activeArticles = document.querySelectorAll("article.active")
     
