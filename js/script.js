242b2318be800ce7743d3ca3b35ecb8347171b68
Krystian Błasiak <blasiakkrystian@o2.pl>
2026-02-04 17:44:29 +0100

    Add remove class 'active' from all articles in function titleClickHandler in file script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
 'use strict';
 
 function titleClickHandler(event){
-    console.log('Link was clicked!');
+    console.log("Link was clicked!");
     console.log(event)
 
     /* remove class 'active' from all article links  */
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
+    const activeLinks = document.querySelectorAll(".titles a.active");
 
     for(let activeLink of activeLinks){
-        activeLink.classList.remove('active');
+        activeLink.classList.remove("active");
     }
 
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
     
+    const activeArticles = document.querySelectorAll("article.active")
+    
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove("active")
+    }
+
     /* get 'href' attribute from the clicked link */
 
     /* find the correct article using the selector (value of 'href' attribute) */
