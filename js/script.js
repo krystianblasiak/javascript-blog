9152733a73640c0fe7cbbb8868e948f6c98bf0cc
Krystian Błasiak <blasiakkrystian@o2.pl>
2026-02-12 15:18:23 +0100

    Add function generateTagsRightColumn to file script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles',
     optArticleTagsSelector = ".post-tags .list",
-    optArticleAuthorSelector = ".post-author";
+    optArticleAuthorSelector = ".post-author",
+    optTagsListSelector = ".tags.list";
 
 function titleClickHandler(event){
     event.preventDefault();
@@ -263,3 +264,58 @@ function addClickListenersToAuthors(){
 }
 
 addClickListenersToAuthors();
+
+function generateTagsRightColumn(){
+    /* [NEW] create a new variable allTags with an empty array */
+    let allTags = [];
+
+    /* find all articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    /* START LOOP: for every article: */
+    for(let article of articles){
+
+        /* find tags wrapper */
+        const wrapperTags = document.querySelectorAll(optTagsListSelector);
+
+        /* make html variable with empty string */
+        let html = "";
+
+        /* get tags from data-tags attribute */
+        const dataTags = article.getAttribute("data-tags");
+
+        /* split tags into array */
+        const tagsArray = dataTags.split(' ');
+
+        /* START LOOP: for each tag */
+        for(let tag of tagsArray){
+
+            /* generate HTML of the link */
+            const linkHTML = "<li><a href='#tag-" + tag + "'>"+ tag +"</a></li>\n";
+
+            /* add generated code to html variable */
+            html += linkHTML;
+
+            /* [NEW] check if this link is NOT already in allTags */
+            if(allTags.indexOf(linkHTML) == -1){
+                /* [NEW] add generated code to allTags array */
+                allTags.push(linkHTML);
+            }
+
+        /* END LOOP: for each tag */
+        }
+
+        /* insert HTML of all the links into the tags wrapper */
+        wrapperTags.innerHTML = html;
+
+    /* END LOOP: for every article: */
+    }
+
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector(optTagsListSelector);
+
+    /* [NEW] add html from allTags to tagList */
+    tagList.innerHTML = allTags.join(' ');
+}
+
+generateTagsRightColumn();
